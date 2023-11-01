c7ef048a43365c1f3e54be3af540d77f8cadefe2
AleksGF <aleksgf@mail.zp.ua>
2023-11-01 11:39:23 +0200

    refactor: refactor ContentItem component

--- src/components/ContentFrame/ContentItem/ContentItem.tsx
+++ src/components/ContentFrame/ContentItem/ContentItem.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { type FC } from 'react';
 import type { ContentItemProps } from '../../../types/types';
 import './ContentItem.scss';
 
-class ContentItem extends React.Component<
-  React.PropsWithoutRef<ContentItemProps>,
-  undefined
-> {
-  render() {
-    return (
-      <div className={'content-item__wrapper'}>
-        <div className={'content-item__name'}>{this.props.person.name}</div>
-        <div className={'content-item__description'}>{`Was born in ${
-          this.props.person.birth_year || 'n/a'
-        }, gender: ${this.props.person.gender || 'n/a'}`}</div>
-      </div>
-    );
-  }
-}
+const ContentItem: FC<ContentItemProps> = (props) => {
+  const { person } = props;
+
+  return (
+    <div className={'content-item__wrapper'}>
+      <div className={'content-item__name'}>{person.name}</div>
+      <div className={'content-item__description'}>{`Was born in ${
+        person.birth_year || 'n/a'
+      }, gender: ${person.gender || 'n/a'}`}</div>
+    </div>
+  );
+};
 
 export default ContentItem;
