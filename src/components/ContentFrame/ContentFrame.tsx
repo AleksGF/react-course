cc658742ed4f4796197f6ffe642951732658e766
AleksGF <aleksgf@mail.zp.ua>
2023-11-01 11:41:50 +0200

    refactor: refactor ContentFrame component

--- src/components/ContentFrame/ContentFrame.tsx
+++ src/components/ContentFrame/ContentFrame.tsx
@@ -1,32 +1,22 @@
-import React from 'react';
+import React, { type FC } from 'react';
 import ContentItem from './ContentItem/ContentItem';
 import type { ContentFrameProps } from '../../types/types';
 import './ContentFrame.scss';
 
-class ContentFrame extends React.Component<
-  React.PropsWithoutRef<ContentFrameProps>,
-  undefined
-> {
-  render() {
-    if (this.props.people.length === 0)
-      return (
-        <div className={'content-frame__wrapper'}>
-          <h2>There are no persons to display</h2>
-        </div>
-      );
+const ContentFrame: FC<ContentFrameProps> = (props) => {
+  const { people } = props;
 
-    return (
-      <div className={'content-frame__wrapper'}>
-        <h2 className={'content-frame__title'}>Star War Persons</h2>
-        {this.props.people.map((person) => (
-          <ContentItem
-            key={`${person.name}-${person.url}-${person.created}`}
-            person={person}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={'content-frame__wrapper'}>
+      <h2 className={'content-frame__title'}>Star War Persons</h2>
+      {people.map((person) => (
+        <ContentItem
+          key={`${person.name}-${person.url}-${person.created}`}
+          person={person}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default ContentFrame;
