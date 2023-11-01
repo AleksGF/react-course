3475f4d6ca176dc91151c866b9884e596efef40c
AleksGF <aleksgf@mail.zp.ua>
2023-11-01 11:21:50 +0200

    refactor: refactor Loader component

--- src/components/common/Loader/Loader.tsx
+++ src/components/common/Loader/Loader.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { type FC } from 'react';
 import './Loader.scss';
 
-class Loader extends React.Component<undefined, undefined> {
-  render() {
-    return (
-      <div className={'loader__wrapper'}>
-        <div className="loader__container">
-          <svg className="loader__circle" viewBox="25 25 50 50">
-            <circle
-              className="path"
-              cx="50"
-              cy="50"
-              r="20"
-              fill="none"
-              strokeWidth="5"
-              strokeMiterlimit="10"
-            />
-          </svg>
-        </div>
+const Loader: FC = () => {
+  return (
+    <div className={'loader__wrapper'}>
+      <div className={'loader__container'}>
+        <svg className={'loader__circle'} viewBox={'25 25 50 50'}>
+          <circle
+            className={'path'}
+            cx={'50'}
+            cy={'50'}
+            r={'20'}
+            fill={'none'}
+            strokeWidth={'5'}
+            strokeMiterlimit={'10'}
+          />
+        </svg>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Loader;
