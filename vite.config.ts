17f8aa421575e3be93e9440dad242d48a6fb1acc
AleksGF <aleksgf@mail.zp.ua>
2023-11-08 11:48:34 +0200

    feat: add file and alias for constants

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@components': path.resolve(__dirname, './src/components'),
       '@assets': path.resolve(__dirname, './src/assets'),
+      '@constants': path.resolve(__dirname, './src/constants'),
       '@services': path.resolve(__dirname, './src/services'),
       '@helpers': path.resolve(__dirname, './src/helpers'),
       '@types': path.resolve(__dirname, './src/types'),
